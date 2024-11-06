cddf2effc447f6322378624a59c80582082293b7
unknown <niki.vasvi@gmail.com>
2024-11-06 22:31:40 +0200

    home view

--- NVMotors/obj/Debug/net8.0/NVMotors.Web.AssemblyInfo.cs
+++ NVMotors/obj/Debug/net8.0/NVMotors.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NVMotors.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653f5d6b541fa0989df480eaef26d6adeb0cb035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f82e872d4d39cf3a8e277bb026dcbfd72e0b4807")]
 [assembly: System.Reflection.AssemblyProductAttribute("NVMotors.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NVMotors.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
