8d9bed0c9e29e04b011ed8cb5741563040641125
unknown <niki.vasvi@gmail.com>
2024-12-09 13:14:34 +0200

    query modal

---
+++ NVMotors.Web.ViewModels/Query/MakeQueryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using NVMotors.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NVMotors.Common.Constants;
+
+namespace NVMotors.Web.ViewModels.Query
+{
+    public class MakeQueryViewModel
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [RegularExpression(@"^(\+359|0)\d{9}$", ErrorMessage = "Please enter valid phone number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        [MinLength(DescriptionMinLengt)]
+        [MaxLength(DescriptionMaxLengt)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [ForeignKey(nameof(AdId))]
+        public Guid AdId { get; set; }
+        public Guid RequesterId { get; set; }
+    }
+}
--- NVMotors.Web.ViewModels/obj/Debug/net8.0/NVMotors.Web.ViewModels.AssemblyInfo.cs
+++ NVMotors.Web.ViewModels/obj/Debug/net8.0/NVMotors.Web.ViewModels.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NVMotors.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8205664e1797307677a8d147bca6a82cea5326a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc186da7efe0f0f8362cedc2d0fed6f044c52fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("NVMotors.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NVMotors.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
